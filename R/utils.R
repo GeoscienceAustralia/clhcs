a14f0ae5ffc6c2e00a699bea8ded3f812e6cefc3
Sudipta Basak <basaks@gmail.com>
2023-05-17 16:11:18 +1000

    use vectorised find interval to find quantiles

--- R/utils.R
+++ R/utils.R
@@ -17,11 +17,11 @@ generate_covariate_quantile_matrix <- function(covariate_data, number_of_bins) {
   # Check that the input arguments are valid
   stopifnot(is.data.frame(covariate_data))
   stopifnot(is.numeric(number_of_bins))
-  tolerance <- 1e-10
+  tolerance <- 1e-10  # this little bit of tolerance makes sure the covariate points don't fall outside when compared
   quantiles <- matrix(NA, nrow = (number_of_bins + 1), ncol = ncol(covariate_data))
   for (i in 1:ncol(covariate_data)) {
     # get a quantile matrix together of the covariates
-    cov_range <- max(covariate_data[, i]) - min(covariate_data[, i]) + 2*tolerance
+    cov_range <- max(covariate_data[, i]) - min(covariate_data[, i]) + 2 * tolerance
     cov_step <- cov_range / number_of_bins
     quantiles[, i] <- seq(min(covariate_data[, i]) - tolerance, to = max(covariate_data[, i]) + tolerance, by = cov_step)
   }
@@ -53,20 +53,20 @@ generate_hypercube <- function(covariate_data, quantiles) {
 }
 
 generate_hypercube_vec <- function(covariate_data, quantiles) {
-    stopifnot(is.data.frame(covariate_data))
-    stopifnot(ncol(covariate_data) == ncol(quantiles))
-    number_of_bins <- nrow(quantiles) - 1
-    hypercube <- matrix(0, nrow = number_of_bins, ncol = ncol(covariate_data))
-    for (i in 1:nrow(covariate_data)) { # the number of pixels
-      for (j in 1:ncol(covariate_data)) { # for each column/covariate
-        dd <- covariate_data[i, j]
-        hypercube[findInterval(dd, quantiles[, j]), j] <- hypercube[findInterval(dd, quantiles[, j]), j] + 1
-      }
+  stopifnot(is.data.frame(covariate_data))
+  stopifnot(ncol(covariate_data) == ncol(quantiles))
+  number_of_bins <- nrow(quantiles) - 1
+  hypercube <- matrix(0, nrow = number_of_bins, ncol = ncol(covariate_data))
+  for (i in 1:nrow(covariate_data)) { # the number of pixels
+    for (j in 1:ncol(covariate_data)) { # for each column/covariate
+      dd <- covariate_data[i, j]
+      hypercube[findInterval(dd, quantiles[, j]), j] <- hypercube[findInterval(dd, quantiles[, j]), j] + 1
     }
+  }
 
-    # Replace zeros with a small number so we don't have to deal with zeros
-    hypercube[which(hypercube == 0)] <- 0.000001
-    return(hypercube)
+  # Replace zeros with a small number so we don't have to deal with zeros
+  hypercube[which(hypercube == 0)] <- 0.000001
+  return(hypercube)
 }
 
 
@@ -78,13 +78,7 @@ composite_from_quantiles <- function(covariate_data, quantiles) {
   for (i in 1:nrow(covariate_data)) { # the number of pixels
     for (j in 1:ncol(covariate_data)) { # for each column/covariate
       dd <- covariate_data[i, j]
-      for (k in 1:number_of_bins) {  # for each quantile
-        kl <- quantiles[k, j]
-        ku <- quantiles[k + 1, j]
-        if (dd >= kl & dd <= ku) {
-          hypercube[i, j] <- k
-        }
-      }
+      hypercube[i, j] <- findInterval(dd, quantiles[, j])
     }
   }
   return(hypercube)
